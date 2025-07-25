73e97d1275ee26f91f400b3acf0bf5e6af007605
Robin Custance <robincustance@gmail.com>
2025-07-25 09:39:28 +0930

    Quick update push — 2025-07-25 09:39 AM

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 # gunicorn.conf.py
 
 bind = "127.0.0.1:7777"  # Bind to IP address and port
-workers = 8               # Number of worker processes
+workers = 2               # Number of worker processes
 timeout = 120             # Timeout for workers
 loglevel = "info"         # Logging level
 accesslog = "/home/art/logs/gunicorn/gunicorn-access.log"  # Access log path
